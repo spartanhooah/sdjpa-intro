1997cb5303123b96b9b692f3f6861349e6ca11d0
Ben Frey <spartanhooah@gmail.com>
2025-01-29 12:22:02 -0500

    add configuration for wrapper and dependencyUpdates tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
 	groovy
 	id("org.springframework.boot") version "3.4.2"
 	id("io.spring.dependency-management") version "1.1.7"
 	id("io.freefair.lombok") version "8.12"
 	id("com.diffplug.spotless") version "7.0.2"
+	id("com.github.ben-manes.versions") version "0.52.0"
 }
 
 group = "net.frey"
@@ -26,11 +29,29 @@ dependencies {
 	runtimeOnly("com.h2database:h2")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.spockframework:spock-core:2.4-M4-groovy-4.0")
+	testImplementation("org.spockframework:spock-core:2.4-M5-groovy-4.0")
 
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+fun isNonStable(version: String): Boolean {
+	val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+	val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+	val isStable = stableKeyword || regex.matches(version)
+	return isStable.not()
+}
+
+tasks.withType<DependencyUpdatesTask> {
+	rejectVersionIf {
+		isNonStable(candidate.version)
+	}
+}
+
+tasks.wrapper {
+	gradleVersion = "8.12.1"
+	distributionType = Wrapper.DistributionType.ALL
 }
