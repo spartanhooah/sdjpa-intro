a54e1f4633e5dad78e0c184288a674753cc29240
Ben Frey <spartanhooah@gmail.com>
2025-02-07 09:38:58 -0500

    add AuthorUuid and BookUuid unit tests

--- src/test/groovy/net/frey/sdjpa_intro/MySQLIntegrationTest.groovy
+++ src/test/groovy/net/frey/sdjpa_intro/MySQLIntegrationTest.groovy
@@ -1,6 +1,10 @@
 package net.frey.sdjpa_intro
 
+import net.frey.sdjpa_intro.entity.AuthorUuid
+import net.frey.sdjpa_intro.entity.BookUuid
+import net.frey.sdjpa_intro.repository.AuthorUuidRepository
 import net.frey.sdjpa_intro.repository.BookRepository
+import net.frey.sdjpa_intro.repository.BookUuidRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
@@ -15,6 +19,12 @@ class MySQLIntegrationTest extends Specification {
     @Autowired
     BookRepository bookRepository
 
+    @Autowired
+    AuthorUuidRepository authorUuidRepository
+
+    @Autowired
+    BookUuidRepository bookUuidRepository
+
     def "test a transaction"() {
         when:
         def count = bookRepository.count()
@@ -23,4 +33,39 @@ class MySQLIntegrationTest extends Specification {
         count == 2
     }
 
+    def "save and retrieve an AuthorUuid"() {
+        given:
+        def author = new AuthorUuid(firstName: "Nora", lastName: "Jemisin")
+
+        when:
+        def savedAuthor = authorUuidRepository.save(author)
+
+        then:
+        authorUuidRepository.count() == 2
+
+        and:
+        when:
+        def foundAuthor = authorUuidRepository.findById(savedAuthor.id).get()
+
+        then:
+        foundAuthor.lastName == "Jemisin"
+    }
+
+    def "save and retrieve a BookUuid"() {
+        given:
+        def book = new BookUuid(title: "The Fifth Season")
+
+        when:
+        def savedBook = bookUuidRepository.save(book)
+
+        then:
+        bookUuidRepository.count() == 2
+
+        and:
+        when:
+        def foundBook = bookUuidRepository.findById(savedBook.id).get()
+
+        then:
+        foundBook.title == "The Fifth Season"
+    }
 }
