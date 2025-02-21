8b9b08f011e66ea5c52e3c69676cd2810183dfc6
Ben Frey <spartanhooah@gmail.com>
2025-02-20 20:31:59 -0500

    delete Spring Data JPA repositories since this branch doesn't use them

--- src/main/java/net/frey/sdjpa_intro/repository/AuthorRepository.java
+++
@@ -1,6 +0,0 @@
-package net.frey.sdjpa_intro.repository;
-
-import net.frey.sdjpa_intro.entity.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {}
